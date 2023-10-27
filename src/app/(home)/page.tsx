b7a7a1d9d789ac0ee5bef02ca86b43d757d2c4db
jenedua <jenasha29@gmail.com>
2023-10-27 07:34:34 -0300

    feat: add second banner to home page

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -16,7 +16,7 @@ export default async function Home() {
   });
   return (
     <div className="">
-      <Image 
+      <Image
         src="/banner-home-01.png"
         height={0}
         width={0}
@@ -25,16 +25,23 @@ export default async function Home() {
         alt="Até 50% de desconto esse mês"
       />
       <div className="mt-8 px-5">
-      <Categories/>
+        <Categories />
       </div>
-      
+
       <div className="mt-8 px-5">
+        <p className=" mb-3 pl-5 font-bold uppercase">Ofertas</p>
         <ProductList products={deals} />
       </div>
-
-
+      <Image
+        src="/banner-home-02.png"
+        height={0}
+        width={0}
+        className="h-auto w-full"
+        sizes="100vw"
+        alt="Até 55% de desconto em mouses"
+      />
     </div>
-    );
+  );
   
   
 }
