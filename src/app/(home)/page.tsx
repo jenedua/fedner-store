018229e5de567ce34843272f91631009e15c6fde
jenedua <jenasha29@gmail.com>
2023-10-27 19:24:00 -0300

    feat: add third  PromoBanner to home page

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -26,26 +26,34 @@ export default async function Home() {
   });
 
   return (
-    <div className="">
+    <div className="flex flex-col gap-8">
       <PromoBanner
         src="/banner-home-01.png"
         alt="Até 50% de desconto esse mês"
       />
-      <div className="mt-8 px-5">
+      <div className="px-5">
         <Categories />
       </div>
 
-      <div className="mt-8 px-5">
+      <div>
         <SectionTitle>Ofertas</SectionTitle>
         <ProductList products={deals} />
       </div>
+
       <PromoBanner
         src="/banner-home-02.png"
         alt="Até 55% de desconto em mouses"
       />
-      <div className="mt-8 px-5">
+      <div>
         <SectionTitle>Teclados</SectionTitle>
         <ProductList products={keyboards} />
+      </div>
+      <div>
+
+      <PromoBanner
+        src="/banner-home-03.png"
+        alt="Até 55% de desconto em mouses"
+      />
       </div>
     </div>
   );
