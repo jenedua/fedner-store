b6479bdbe85f3f56802149c90a3df1d44f93066a
jenedua <jenasha29@gmail.com>
2023-10-28 16:05:45 -0300

    feat: add Footer components

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from 'next/font/google'
 import './globals.css'
 import Header from '@/components/ui/header'
 import { AuthProvider } from '@/providers/auth'
+import Footer from '@/components/ui/footer'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -23,6 +24,7 @@ export default function RootLayout({
 
         <Header />
         {children}
+        <Footer />
         </AuthProvider>
         </body>
     </html>
