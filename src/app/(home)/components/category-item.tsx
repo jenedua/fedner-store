dd64513d3db0b180be05aea9c7bd2db9c88bb1c6
jenedua <jenasha29@gmail.com>
2023-10-29 16:17:18 -0300

    feat: ensure user goes to category page on category item click

--- src/app/(home)/components/category-item.tsx
+++ src/app/(home)/components/category-item.tsx
@@ -1,6 +1,7 @@
 import { Badge } from "@/components/ui/badge";
 import { CATEGORY_ICON } from "@/constants/category-icon";
 import { Category } from "@prisma/client";
+import Link from "next/link";
 // import { HeadphonesIcon, KeyboardIcon, MonitorIcon, MouseIcon, SpeakerIcon, SquareIcon } from "lucide-react";
 
 interface CategoryItemProps{
@@ -18,13 +19,15 @@ const CategoryItem= ({category}: CategoryItemProps) =>{
 
 
     return (
-      <Badge
-        variant="outline"
-        className="flex items-center justify-center gap-2 py-3"
-      >
-        {CATEGORY_ICON[category.slug as keyof typeof CATEGORY_ICON]}
-        <span className="text-xs font-bold">{category.name}</span>
-      </Badge>
+      <Link href={`/category/${category.slug}`}>
+        <Badge
+          variant="outline"
+          className="flex items-center justify-center gap-2 py-3"
+        >
+          {CATEGORY_ICON[category.slug as keyof typeof CATEGORY_ICON]}
+          <span className="text-xs font-bold">{category.name}</span>
+        </Badge>
+      </Link>
     );
    
 };
